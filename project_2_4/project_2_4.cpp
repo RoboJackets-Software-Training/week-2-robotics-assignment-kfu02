88af391f29a608975b98978725bb0946eeecdbaa
vagrant <kevin.fu842@gmail.com>
2020-09-30 20:32:30 +0000

    rm class headers

--- project_2_4/project_2_4.cpp
+++ project_2_4/project_2_4.cpp
@@ -19,70 +19,64 @@ struct LightInputs {
 // TODO Define your light controller state machine class here
 class LightController {
 public:
-  LightController();
+  LightController() {
+    my_state = "red";
+    remaining_time = 1;
+    // equivalent to changeState("red");
+  }
 
-  LightOutputs update(LightInputs input);
+  LightOutputs update(LightInputs input) {
+    if (remaining_time == 0) { // when no time remaining, change to proper next state
+      if (my_state == "red") {
+        if (input.car_sensor_n || input.car_sensor_s) {
+          changeState("ns_green");
+        } else {
+          changeState("ew_green");
+        }
+      } else if (my_state == "ew_green") {
+        changeState("ew_yellow");
+      } else if (my_state == "ns_green") {
+        changeState("ns_yellow");
+      } else if (my_state == "ew_yellow" || my_state == "ns_yellow") {
+        changeState("red");
+      }
+    }
+    // decrement time, return current state in LightOutput form
+    remaining_time--;
+    return translateMyState();
+  }
 
 private:
   std::string my_state;
   int remaining_time; // before my_state changes
 
-  void changeState(std::string new_state);
-  LightOutputs translateMyState();
-};
-
-LightController::LightController() {
-  my_state = "red";
-  remaining_time = 1;
-  // equivalent to changeState("red");
-}
-
-LightOutputs LightController::update(LightInputs input) {
-  if (remaining_time == 0) { // when no time remaining, change to proper next state
-    if (my_state == "red") {
-      if (input.car_sensor_n || input.car_sensor_s) {
-        changeState("ns_green");
-      } else {
-        changeState("ew_green");
-      }
-    } else if (my_state == "ew_green") {
-      changeState("ew_yellow");
-    } else if (my_state == "ns_green") {
-      changeState("ns_yellow");
+  // update state, remaining time based on new state
+  void changeState(std::string new_state) {
+    my_state = new_state;
+    remaining_time = 1; // assume "red"
+    if (new_state == "ew_green" || new_state == "ns_green") {
+      remaining_time = 5;
     } else if (my_state == "ew_yellow" || my_state == "ns_yellow") {
-      changeState("red");
+      remaining_time = 2;
     }
   }
-  // decrement time, return current state in LightOutput form
-  remaining_time--;
-  return translateMyState();
-}
 
-//update state, remaining time
-void LightController::changeState(std::string new_state) {
-  my_state = new_state;
-  remaining_time = 1; // assume "red"
-  if (new_state == "ew_green" || new_state == "ns_green") {
-    remaining_time = 5;
-  } else if (my_state == "ew_yellow" || my_state == "ns_yellow") {
-    remaining_time = 2;
+  // turn my_state str into binary output
+  LightOutputs translateMyState() {
+      if (my_state == "red") {
+          return {true, false, false, true, false, false};
+      } else if (my_state == "ew_green") {
+          return {true, false, false, false, false, true};
+      } else if (my_state == "ew_yellow") {
+          return {true, false, false, false, true, false};
+      } else if (my_state == "ns_green") {
+          return {false, false, true, true, false, false};
+      } else if (my_state == "ns_yellow") {
+          return {false, true, false, true, false, false};
+      }
+      // no other my_state options, ignore compiler warning
   }
-}
-
-LightOutputs LightController::translateMyState() {
-    if (my_state == "red") {
-        return {true, false, false, true, false, false};
-    } else if (my_state == "ew_green") {
-        return {true, false, false, false, false, true};
-    } else if (my_state == "ew_yellow") {
-        return {true, false, false, false, true, false};
-    } else if (my_state == "ns_green") {
-        return {false, false, true, true, false, false};
-    } else if (my_state == "ns_yellow") {
-        return {false, true, false, true, false, false};
-    }
-    // no other my_state options, ignore compiler warning
-}
+};
 
 int main()
 {
